6a239bbd6a6642f02bba48b68d228467959b6ac9
EldarMemetov <eldarm9347@gmail.com>
2025-03-26 19:16:14 +0100

    test

--- src/modules/TextAnimation/TextAnimation.jsx
+++ src/modules/TextAnimation/TextAnimation.jsx
@@ -130,79 +130,79 @@
 //     </section>
 //   );
 // }
-// 'use client';
-// import { useEffect, useRef } from 'react';
-// import { gsap } from 'gsap';
-// import { ScrollTrigger } from 'gsap/ScrollTrigger';
-// import s from './TextAnimation.module.scss';
-// import Container from '@/shared/container/Container';
+'use client';
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import s from './TextAnimation.module.scss';
+import Container from '@/shared/container/Container';
 
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-// export default function TextAnimation() {
-//   const sectionRef = useRef(null);
-//   const textRefs = useRef([]);
+export default function TextAnimation() {
+  const sectionRef = useRef(null);
+  const textRefs = useRef([]);
 
-//   useEffect(() => {
-//     const elements = textRefs.current;
+  useEffect(() => {
+    const elements = textRefs.current;
 
-//     let tl = gsap.timeline({
-//       scrollTrigger: {
-//         trigger: sectionRef.current,
-//         start: 'top top',
-//         end: '+=300%',
-//         scrub: true,
-//         pin: true,
-//       },
-//     });
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: sectionRef.current,
+        start: 'top top',
+        end: '+=300%',
+        scrub: true,
+        pin: true,
+      },
+    });
 
-//     // "Бум" эффект для текста, без размытия
-//     elements.forEach((el, index) => {
-//       if (index > 0) {
-//         tl.to(elements[index - 1], {
-//           opacity: 0,
-//           scale: 0.8,
-//           duration: 0.5,
-//         })
-//           .to(el, {
-//             opacity: 1,
-//             scale: 1.2, // эффект "бум"
-//             duration: 0.5,
-//             ease: 'back.out(1.7)', // для плавного отскока
-//           })
-//           .to(el, {
-//             scale: 1, // возвращаем в нормальное состояние
-//             duration: 0.3,
-//           });
-//       }
-//     });
+    // "Бум" эффект для текста, без размытия
+    elements.forEach((el, index) => {
+      if (index > 0) {
+        tl.to(elements[index - 1], {
+          opacity: 0,
+          scale: 0.8,
+          duration: 0.5,
+        })
+          .to(el, {
+            opacity: 1,
+            scale: 1.2, // эффект "бум"
+            duration: 0.5,
+            ease: 'back.out(1.7)', // для плавного отскока
+          })
+          .to(el, {
+            scale: 1, // возвращаем в нормальное состояние
+            duration: 0.3,
+          });
+      }
+    });
 
-//     return () => {
-//       ScrollTrigger.getAll().forEach((st) => st.kill());
-//     };
-//   }, []);
+    return () => {
+      ScrollTrigger.getAll().forEach((st) => st.kill());
+    };
+  }, []);
 
-//   return (
-//     <section ref={sectionRef} className={s.section}>
-//       <Container>
-//         <div className={s.textContainer}>
-//           <div
-//             ref={(el) => (textRefs.current[0] = el)}
-//             className={`${s.text} ${s.active}`}
-//           >
-//             <h2 className={s.textInterested}>зацікавило?</h2>
-//           </div>
-//           <div ref={(el) => (textRefs.current[1] = el)} className={s.text}>
-//             <h2 className={s.textGo}>тоді давай</h2>
-//           </div>
-//           <div ref={(el) => (textRefs.current[2] = el)} className={s.text}>
-//             <h2 className={s.textWork}>співпрацювати</h2>
-//           </div>
-//         </div>
-//       </Container>
-//     </section>
-//   );
-// }
+  return (
+    <section ref={sectionRef} className={s.section}>
+      <Container>
+        <div className={s.textContainer}>
+          <div
+            ref={(el) => (textRefs.current[0] = el)}
+            className={`${s.text} ${s.active}`}
+          >
+            <h2 className={s.textInterested}>зацікавило?</h2>
+          </div>
+          <div ref={(el) => (textRefs.current[1] = el)} className={s.text}>
+            <h2 className={s.textGo}>тоді давай</h2>
+          </div>
+          <div ref={(el) => (textRefs.current[2] = el)} className={s.text}>
+            <h2 className={s.textWork}>співпрацювати</h2>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+}
 // 'use client';
 // import { useEffect, useRef } from 'react';
 // import { gsap } from 'gsap';
@@ -341,75 +341,75 @@
 //     </section>
 //   );
 // }
-'use client';
-import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import s from './TextAnimation.module.scss';
-import Container from '@/shared/container/Container';
+// 'use client';
+// import { useEffect, useRef } from 'react';
+// import { gsap } from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// import s from './TextAnimation.module.scss';
+// import Container from '@/shared/container/Container';
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-export default function TextAnimation() {
-  const sectionRef = useRef(null);
-  const textRefs = useRef([]);
+// export default function TextAnimation() {
+//   const sectionRef = useRef(null);
+//   const textRefs = useRef([]);
 
-  useEffect(() => {
-    const elements = textRefs.current;
+//   useEffect(() => {
+//     const elements = textRefs.current;
 
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: sectionRef.current,
-        start: 'top top',
-        end: '+=100%', // Уменьшаем диапазон скролла, чтобы анимация произошла быстрее
-        scrub: true,
-        pin: true,
-      },
-    });
+//     let tl = gsap.timeline({
+//       scrollTrigger: {
+//         trigger: sectionRef.current,
+//         start: 'top top',
+//         end: '+=100%', // Уменьшаем диапазон скролла, чтобы анимация произошла быстрее
+//         scrub: true,
+//         pin: true,
+//       },
+//     });
 
-    // Анимация появления текста с мягким бум эффектом
-    elements.forEach((el, index) => {
-      if (index > 0) {
-        tl.to(elements[index - 1], {
-          opacity: 0,
-          duration: 0.3, // Уменьшаем продолжительность исчезновения
-        })
-          .to(el, {
-            opacity: 1,
-            scale: 1.1, // Слабый бум эффект (увеличение на 10%)
-            duration: 0.3, // Уменьшаем продолжительность появления
-            ease: 'back.out(0.2)', // Мягкий эффект отскока
-          })
-          .to(el, {
-            scale: 1, // Возвращаем в нормальное состояние
-            duration: 0.3,
-          });
-      }
-    });
+//     // Анимация появления текста с мягким бум эффектом
+//     elements.forEach((el, index) => {
+//       if (index > 0) {
+//         tl.to(elements[index - 1], {
+//           opacity: 0,
+//           duration: 0.3, // Уменьшаем продолжительность исчезновения
+//         })
+//           .to(el, {
+//             opacity: 1,
+//             scale: 1.1, // Слабый бум эффект (увеличение на 10%)
+//             duration: 0.3, // Уменьшаем продолжительность появления
+//             ease: 'back.out(0.2)', // Мягкий эффект отскока
+//           })
+//           .to(el, {
+//             scale: 1, // Возвращаем в нормальное состояние
+//             duration: 0.3,
+//           });
+//       }
+//     });
 
-    return () => {
-      ScrollTrigger.getAll().forEach((st) => st.kill());
-    };
-  }, []);
+//     return () => {
+//       ScrollTrigger.getAll().forEach((st) => st.kill());
+//     };
+//   }, []);
 
-  return (
-    <section ref={sectionRef} className={s.section}>
-      <Container>
-        <div className={s.textContainer}>
-          <div
-            ref={(el) => (textRefs.current[0] = el)}
-            className={`${s.text} ${s.active}`}
-          >
-            <h2 className={s.textInterested}>зацікавило?</h2>
-          </div>
-          <div ref={(el) => (textRefs.current[1] = el)} className={s.text}>
-            <h2 className={s.textGo}>тоді давай</h2>
-          </div>
-          <div ref={(el) => (textRefs.current[2] = el)} className={s.text}>
-            <h2 className={s.textWork}>співпрацювати</h2>
-          </div>
-        </div>
-      </Container>
-    </section>
-  );
-}
+//   return (
+//     <section ref={sectionRef} className={s.section}>
+//       <Container>
+//         <div className={s.textContainer}>
+//           <div
+//             ref={(el) => (textRefs.current[0] = el)}
+//             className={`${s.text} ${s.active}`}
+//           >
+//             <h2 className={s.textInterested}>зацікавило?</h2>
+//           </div>
+//           <div ref={(el) => (textRefs.current[1] = el)} className={s.text}>
+//             <h2 className={s.textGo}>тоді давай</h2>
+//           </div>
+//           <div ref={(el) => (textRefs.current[2] = el)} className={s.text}>
+//             <h2 className={s.textWork}>співпрацювати</h2>
+//           </div>
+//         </div>
+//       </Container>
+//     </section>
+//   );
+// }
