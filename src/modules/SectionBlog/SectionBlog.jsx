f79582b970d7676080d436efc384075abf8253de
EldarMemetov <eldarm9347@gmail.com>
2025-04-17 11:36:31 +0200

    test

--- src/modules/SectionBlog/SectionBlog.jsx
+++ src/modules/SectionBlog/SectionBlog.jsx
@@ -1,7 +1,8 @@
 import Container from '@/shared/container/Container';
 import { initServerI18n } from '@/i18n/utils/serverI18n';
 import Image from 'next/image';
-import Link from 'next/link';
+import LinkButton from '@/shared/components/LinkButton/LinkButton';
+import { LINKDATA } from '@/shared/constants';
 
 export default async function SectionBlog({ locale }) {
   const { t } = await initServerI18n(locale, ['blog']);
@@ -13,20 +14,25 @@ export default async function SectionBlog({ locale }) {
       <Container>
         <div>
           {Object.entries(blogData).map(([id, post]) => (
-            <Link key={id} href={`/${locale}/blog/${id}`}>
-              <div>
-                {post.image && (
-                  <Image
-                    src={post.image}
-                    alt={post.title}
-                    width={363}
-                    height={383}
-                  />
-                )}
-                <h2>{post.title}</h2>
-                <p>{post.description}</p>
-              </div>
-            </Link>
+            <div key={id}>
+              {post.image && (
+                <Image
+                  src={post.image}
+                  alt={post.title}
+                  width={363}
+                  height={383}
+                />
+              )}
+              <h2>{post.title}</h2>
+              <p>{post.description}</p>
+
+              <LinkButton
+                path={`/${locale}/blog/${id}`}
+                linkText="Читати далі"
+                type={LINKDATA.TYPE_LIGHT_BORDER}
+                className="custom-blog-button"
+              />
+            </div>
           ))}
         </div>
       </Container>
