d729c75ea4ed6268b7939270e17a9a5683d4cb2c
EldarMemetov <eldarm9347@gmail.com>
2025-04-20 08:34:43 +0200

    test

--- src/app/[locale]/not-found.js
+++ src/app/[locale]/not-found.js
@@ -1,33 +1,10 @@
 'use client';
-import dynamic from 'next/dynamic';
-
-const NotFoundPage = dynamic(
-  () => import('@/modules/NotFoundPage/NotFoundPage'),
-  { ssr: false }
-);
+import NotFoundPage from '@/modules/NotFoundPage/NotFoundPage';
 
 export default function NotFound() {
-  return <NotFoundPage />;
+  return (
+    <div>
+      <NotFoundPage />
+    </div>
+  );
 }
-
-// 'use client';
-// import { useTranslation } from 'react-i18next';
-// import Link from 'next/link';
-
-// import s from './notFoundPage.module.scss';
-
-// export default function NotFoundPage() {
-//   const { t } = useTranslation('errorBoundary');
-
-//   return (
-//     <section className={s.section}>
-//       <div className={s.container}>
-//         <h2 className={s.title}>{t('notFoundTitle')}</h2>
-//         <p className={s.message}>{t('notFoundMessage')}</p>
-//         <Link href="/" className={s.link}>
-//           {t('backToHome')}
-//         </Link>
-//       </div>
-//     </section>
-//   );
-// }
--- src/modules/NotFoundPage/NotFoundPage.jsx
+++ src/modules/NotFoundPage/NotFoundPage.jsx
@@ -3,19 +3,22 @@ import { useTranslation } from 'react-i18next';
 import Link from 'next/link';
 
 import s from './NotFoundPage.module.scss';
+import Container from '@/shared/container/Container';
 
 export default function NotFoundPage() {
   const { t } = useTranslation('errorBoundary');
 
   return (
     <section className={s.section}>
-      <div className={s.container}>
-        <h2 className={s.title}>{t('notFoundTitle')}</h2>
-        <p className={s.message}>{t('notFoundMessage')}</p>
-        <Link href="/" className={s.link}>
-          {t('backToHome')}
-        </Link>
-      </div>
+      <Container>
+        <div className={s.container}>
+          <h2 className={s.title}>{t('notFoundTitle')}</h2>
+          <p className={s.message}>{t('notFoundMessage')}</p>
+          <Link href="/" className={s.link}>
+            {t('backToHome')}
+          </Link>
+        </div>
+      </Container>
     </section>
   );
 }
