1269b4461f07678653877276d2c9a4264bc311e3
EldarMemetov <eldarm9347@gmail.com>
2025-04-20 07:55:54 +0200

    test not found

--- src/app/[locale]/not-found.js
+++ src/app/[locale]/not-found.js
@@ -1,9 +1,30 @@
-import NotFoundPage from '@/modules/NotFoundPage/NotFoundPage';
+// import NotFoundPage from '@/modules/NotFoundPage/NotFoundPage';
 
-export default function NotFound() {
+// export default function NotFound() {
+//   return (
+//     <div>
+//       <NotFoundPage />
+//     </div>
+//   );
+// }
+'use client';
+import { useTranslation } from 'react-i18next';
+import Link from 'next/link';
+
+import s from './notFoundPage.module.scss';
+
+export default function NotFoundPage() {
+  const { t } = useTranslation('errorBoundary');
+
   return (
-    <div>
-      <NotFoundPage />
-    </div>
+    <section className={s.section}>
+      <div className={s.container}>
+        <h2 className={s.title}>{t('notFoundTitle')}</h2>
+        <p className={s.message}>{t('notFoundMessage')}</p>
+        <Link href="/" className={s.link}>
+          {t('backToHome')}
+        </Link>
+      </div>
+    </section>
   );
 }
--- src/modules/NotFoundPage/NotFoundPage.jsx
+++ src/modules/NotFoundPage/NotFoundPage.jsx
@@ -1,21 +1,21 @@
-'use client';
-import { useTranslation } from 'react-i18next';
-import Link from 'next/link';
+// 'use client';
+// import { useTranslation } from 'react-i18next';
+// import Link from 'next/link';
 
-import s from './NotFoundPage.module.scss';
+// import s from './NotFoundPage.module.scss';
 
-export default function NotFoundPage() {
-  const { t } = useTranslation('errorBoundary');
+// export default function NotFoundPage() {
+//   const { t } = useTranslation('errorBoundary');
 
-  return (
-    <section className={s.section}>
-      <div className={s.container}>
-        <h2 className={s.title}>{t('notFoundTitle')}</h2>
-        <p className={s.message}>{t('notFoundMessage')}</p>
-        <Link href="/" className={s.link}>
-          {t('backToHome')}
-        </Link>
-      </div>
-    </section>
-  );
-}
+//   return (
+//     <section className={s.section}>
+//       <div className={s.container}>
+//         <h2 className={s.title}>{t('notFoundTitle')}</h2>
+//         <p className={s.message}>{t('notFoundMessage')}</p>
+//         <Link href="/" className={s.link}>
+//           {t('backToHome')}
+//         </Link>
+//       </div>
+//     </section>
+//   );
+// }
