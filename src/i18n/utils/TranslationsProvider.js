36c952c9b0337460e574a0b0dd10779df6b8d010
EldarMemetov <eldarm9347@gmail.com>
2025-04-20 10:17:54 +0200

    test

--- src/i18n/utils/TranslationsProvider.js
+++ src/i18n/utils/TranslationsProvider.js
@@ -16,13 +16,12 @@
 
 //   return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 // }
+// components/TranslationsProvider.js
 'use client';
-
 import React, { useEffect, useState } from 'react';
 import { I18nextProvider } from 'react-i18next';
-import initTranslations from './i18n';
+import initTranslations from '@/i18n/utils/i18n';
 
-// Гибридная логика: если i18n передали — используем, иначе инициализируем на клиенте
 export default function TranslationsProvider({
   children,
   locale,
@@ -33,6 +32,7 @@ export default function TranslationsProvider({
   const [i18nInstance, setI18nInstance] = useState(serverI18n || null);
 
   useEffect(() => {
+    // Инициализация переводов на клиенте, если их нет
     if (!serverI18n) {
       initTranslations(locale, namespaces, undefined, resources).then(
         ({ i18n }) => {
@@ -40,7 +40,7 @@ export default function TranslationsProvider({
         }
       );
     }
-  }, [locale, serverI18n]);
+  }, [locale, serverI18n, resources, namespaces]);
 
   if (!i18nInstance) return null;
 
