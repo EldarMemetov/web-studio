83a4b07b9228b3efb590ee485303ae07ea2d986f
EldarMemetov <eldarm9347@gmail.com>
2025-04-22 14:29:56 +0200

    fix modal and fix icon

--- src/modules/BrandTransformation/ItemBrand/ItemBrand.jsx
+++ src/modules/BrandTransformation/ItemBrand/ItemBrand.jsx
@@ -3,17 +3,16 @@ import Icon from '@/shared/Icon/Icon';
 export default function ItemBrand({ title, description, iconName }) {
   return (
     <li className={styles.ContainerItem}>
-      <div className={styles.containerIcon}>
-        <h3 className={styles.titleInformation}>{title}</h3>
-        <Icon iconName="icon-game" className={styles.game} />
-      </div>
-      <p className={styles.description}>{description}</p>
       <Icon
         iconName={iconName}
         className={styles.iconStyle}
         width={160}
         height={103}
       />
+
+      <h3 className={styles.titleInformation}>{title}</h3>
+
+      <p className={styles.description}>{description}</p>
     </li>
   );
 }
