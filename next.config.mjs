c4bca2266e1136df46238119b1981ae75753bcb5
EldarMemetov <eldarm9347@gmail.com>
2025-05-25 08:44:20 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,12 @@ const nextConfig = {
     `,
   },
   images: {
-    domains: ['cdn.sanity.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
   },
 };
 
